44b1c8f5822892ee770ea15827468ab8d6e87909
Shibe <juligibelli@gmail.com>
2023-02-06 16:11:59 -0300

    se soluciona addProduct incluyendo await

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import ProductManager from "./pm.mjs";
 
 const manager = new ProductManager("./productos.json");
-manager.addProduct(1, "mouse", "mouse rosa", 50, "imagen", 4);
-manager.addProduct(56, "teclado", "teclado rosa", 70, "imagen", 23);
-manager.addProduct(23, "auricular", "auricular rosa", 60, "imagen", 212);
-manager.getProducts();
+await manager.addProduct(1, "mouse", "mouse rosa", 50, "imagen", 4);
+await manager.addProduct(56, "teclado", "teclado rosa", 70, "imagen", 23);
+await manager.addProduct(23, "auricular", "auricular rosa", 60, "imagen", 212);
+await manager.getProducts();
 
-manager.getProductById(2).then((res) => {
+await manager.getProductById(2).then((res) => {
   console.log(res);
 });
